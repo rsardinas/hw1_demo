400c1cbe96e49398ee627135a0f0f0bae8143c38
rodrigo sardinas <rodrigo.sardinas@gmail.com>
2026-09-01 16:26:28 +0000

    First update

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@ using namespace std;
 
 int main()
 {
+	Starter_Object s1;
+	s1.val1(25);
+	s1.val2(20);
+	cout << s1.val1() << endl;
+	cout << s1.val2() << endl;
 	//create a few starter objects
 	//add some values to test out your values methods
 	//print out all the values for each object to validate your code
